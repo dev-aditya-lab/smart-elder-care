7d88cde2454667c5d6b51037ca2d715872f9bd20
dev-aditya-lab <ad1123itya@gmail.com>
2023-12-10 15:21:45 +0530

    123asd

--- postLogin/contect.php
+++ postLogin/contect.php
@@ -23,27 +23,28 @@
 
             <div class="input-box">
                 <label>Name</label>
-                <input type="text" name="Name" placeholder="Enter your name" required />
+                <input type="text" name="name" placeholder="Enter your name" required />
             </div>
             
             <div class="input-box">
                 <label>Email</label>
-                <input type="email" name="Email" placeholder="Enter your Email" required />
+                <input type="email" name="email" placeholder="Enter your Email" required />
             </div>
 
             <div class="input-box">
                 <label>Phone no</label>
-                <input type="number" name="phone_no" placeholder="Number" required />
+                <input type="number" name="phone_no" placeholder="Number" required maxlength="12"/>
             </div>
             
             <div class="input-box">
                 <label>Subject</label>
-                <input type="text" name="Text" placeholder="Subject" required maxlength="500"/>
+                <input type="text" name="subject" placeholder="Subject" required maxlength="50"/>
             </div>
             
             <div class="input-box">
                 <label>Message</label><br>
-                <textarea name="Message" placeholder="Enter Your Massage" style="height: 50px;width: 100%; outline: none;font-size: 1rem;color: #707070;margin-top: 8px;border: 1px solid #ddd;border-radius: 6px;padding: 0 15px;" required></textarea>
+                <textarea name="message" placeholder="Enter Your Message" style="height: 50px;width: 100%; outline: none;font-size: 1rem;color: #707070;margin-top: 8px;border: 1px solid #ddd;border-radius: 6px;padding: 0 15px;" maxlength="500" required></textarea>
+                <p style="text-align: right; font-size: .7rem; color: lightgray;">Mex:500 character</p>
             </div>
 
            
@@ -60,41 +61,21 @@
 
 
 <?php
-  if (isset($_POST['Register'])) {
+  if (isset($_POST['send'])) {
 
-    $signup_as = $_POST['signup_as'];
     $f_name = $_POST['name'];
     $email  = $_POST['email'];
-    $password  = $_POST['password'];
-    $con_pass = $_POST['conf_pass'];
     $phone = $_POST['phone_no'];
-    $dob  = $_POST['dob'];
-    $gender = $_POST['gender'];
-    $street = $_POST['street'];
-    $landmark = $_POST['landmark'];
-    $country = $_POST['country'];
-    $city  = $_POST['city'];
-    $state = $_POST['state'];
-    $zip = $_POST['zip'];
+    $subject  = $_POST['subject'];
+    $message  = $_POST['message'];
 
-    $result = "SELECT * FROM signup WHERE phone = '$phone' AND password = '$password'";
     $none = mysqli_query($connection,$result);
     $no = mysqli_num_rows($none);
 
-    if($no != 0){
-      echo '<script>alert("User already exist")</script>';
-    }else{
-      if( $password != $con_pass ){
-        echo '<script>alert("Password do not matched")</script>';
-          }else{
-            $data = "INSERT INTO signup VALUES('$signup_as' , '$f_name' ,	'$email' ,	'$password' ,	'$con_pass' ,	'$phone' ,	'$dob' ,	'$gender' ,	'$street' ,	'$landmark' ,	'$country' ,	'$city' ,	'$state' ,'$zip')";
-            $query = mysqli_query($connection, $data);
-            if ($data) {
-            echo '<script>alert("Registered Successfully")</script>';
-          }else{
-          echo '<script>alert("Something Went Wrong!!Please Try Later")</script>';
-        }
-      }
-    }
+      $data = "INSERT INTO contact VALUES('$f_name' ,	'$email' , '$phone' ,	'$subject' ,	'$message' )";
+      $query = mysqli_query($connection, $data);
+      if ($data) {
+      echo '<script>alert("Message Send Successfully")</script>';
+     }
   }
 ?>
